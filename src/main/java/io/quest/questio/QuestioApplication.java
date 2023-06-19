5042033274e2676f4060f79d3171e930617c983e
Samir Sameja <samejasamir@hotmail.com>
2023-06-18 22:58:23 -0400

    seed project

---
+++ src/main/java/io/quest/questio/QuestioApplication.java
@@ -0,0 +1,13 @@
+package io.quest.questio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/quest/questio/QuestioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.quest.questio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
